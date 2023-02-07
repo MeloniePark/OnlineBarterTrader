df8b014caf983c416460b2bd5b9c3193e76dcc6a
lu3 <xn967824@dal.ca>
2023-02-07 17:51:22 -0400

    UI complete and activity build

---
+++ app/src/main/java/com/example/onlinebartertrader/SignUpActivity.java
@@ -0,0 +1,13 @@
+package com.example.onlinebartertrader;
+
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SignUpActivity {
+    EditText email;
+    EditText password;
+    EditText passwordMatch;
+    Button signUp;
+
+
+}

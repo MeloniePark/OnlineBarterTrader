44241e6e4efc3fb73f053198df4044fc98fe468d
EunBeen Park <en261649@dal.ca>
2023-02-17 11:52:11 -0400

    on mainLandingUnitTest, after and before testing initialized.

--- app/src/main/java/com/example/onlinebartertrader/ReceiverLandingPage.java
+++ app/src/main/java/com/example/onlinebartertrader/ReceiverLandingPage.java
@@ -1,4 +1,8 @@
 package com.example.onlinebartertrader;
 
 public class ReceiverLandingPage {
+    static ReceiverLandingPage receiverLandingPage;
+
+
+
 }
--- app/src/test/java/com/example/onlinebartertrader/MainLandingUnitTest.java
+++ app/src/test/java/com/example/onlinebartertrader/MainLandingUnitTest.java
@@ -1,6 +1,21 @@
 package com.example.onlinebartertrader;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
 public class MainLandingUnitTest {
     static MainActivity mainActivity;
+
+    @BeforeClass
+    public static void setup() {
+        mainActivity = new MainActivity();
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        System.gc();
+    }
+
+
 
 }

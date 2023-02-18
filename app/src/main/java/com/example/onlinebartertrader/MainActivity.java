201600d191a48c6e7d0859812e57baae7c19fab2
Ahmed Galal <a.galal@dal.ca>
2023-02-18 12:46:31 -0400

    Data base created (write method, new user).

--- app/src/main/java/com/example/onlinebartertrader/MainActivity.java
+++ app/src/main/java/com/example/onlinebartertrader/MainActivity.java
@@ -31,6 +31,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private TextView textView;  //for reading whatever is in this textelement.
     Button signUpButton;
     Button signInButton;
+    public class User {
+        public String username;
+        public String email;
+
+        public User() {
+            // Default constructor required for calls to DataSnapshot.getValue(User.class)
+        }
+
+        public User(String username, String email) {
+            this.username = username;
+            this.email = email;
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,6 +75,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void writeToFirebase(){
         firebaseDBRef.setValue("Hello it is connected!");
     }
+    public void writeNewUser(String userId, String name, String email) {
+        User user = new User(name, email);
+        String usrs = "Users";
+        //add the values of the rows and columns.
+        firebaseDBRef.setValue(user);
+    }
+
 
     //reading from the database
     private void listenToDataChanges(){

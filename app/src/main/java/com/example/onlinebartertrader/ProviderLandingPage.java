748301683f94c4bb455fe08d466ae1930679fa4d
EunBeen Park <en261649@dal.ca>
2023-02-19 17:30:27 -0400

    provider's landing page refactored and commented

--- app/src/main/java/com/example/onlinebartertrader/ProviderLandingPage.java
+++ app/src/main/java/com/example/onlinebartertrader/ProviderLandingPage.java
@@ -20,12 +20,16 @@ import java.util.ArrayList;
 
 public class ProviderLandingPage extends AppCompatActivity implements View.OnClickListener {
 
+    //firebase
     FirebaseDatabase database;
     DatabaseReference providerDBRef;
+
+    //view for the lists
     ListView providerItemLists;
 
     Button providerPostBtn;
 
+    //arraylists for listview
     ArrayList<String> providerItems = new ArrayList<>();
 
 
@@ -39,17 +43,22 @@ public class ProviderLandingPage extends AppCompatActivity implements View.OnCli
         final ArrayAdapter<String> providerArrAdapter = new ArrayAdapter<String>
                 (ProviderLandingPage.this, android.R.layout.simple_list_item_1, providerItems);
 
-        //
+        //register the views, buttons and other components for the provider landing page.
         providerItemLists = (ListView) findViewById(R.id.providerListProvider);
+        //setting array Adapter for the ListView providerItemLists.
         providerItemLists.setAdapter(providerArrAdapter);
-
         providerPostBtn = findViewById(R.id.providerPostProvider);
+
+        //listens for click of the post button
         providerPostBtn.setOnClickListener(this);
 
+        //Firebase Connection
         database = FirebaseDatabase.getInstance("https://onlinebartertrader-52c04-default-rtdb.firebaseio.com/");
         //creating reference variable inside the databased called "templateUser"
         providerDBRef = database.getReference("templateUser").child("provider").child("goods");
 
+
+        //Firebase data addition, modification, deletion, reading performed through this section.
         providerDBRef.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
@@ -84,5 +93,6 @@ public class ProviderLandingPage extends AppCompatActivity implements View.OnCli
     @Override
     public void onClick(View view) {
         //where we move on to posting provider's goods page.
+        //Functionality will be added in future iteration 
     }
 }

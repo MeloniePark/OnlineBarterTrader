59077463d943a259d85ef88af00522d5f5b55b65
EunBeen Park <en261649@dal.ca>
2023-02-19 17:49:05 -0400

    JUnit tests for three landing page has been commented

--- app/src/test/java/com/example/onlinebartertrader/ReceiverLandingUnitTest.java
+++ app/src/test/java/com/example/onlinebartertrader/ReceiverLandingUnitTest.java
@@ -1,4 +1,22 @@
 package com.example.onlinebartertrader;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
 public class ReceiverLandingUnitTest {
+    static ReceiverLandingPage receiverLandingPage;
+
+    @BeforeClass
+    public static void setup() {
+        receiverLandingPage = new ReceiverLandingPage();
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        System.gc();
+    }
+    //As discussed with TA, due to the fact that there are no logical functions in receiver's
+    // landing page yet, there are no unit tests for the receiver's landing page for this iteration.
+    // As more functional User stories gets assigned in future iteration,
+    // the JUnit tests will be added.
 }

b048d7ba891708518747e55a0efc09ce15ebfe22
EunBeen Park <en261649@dal.ca>
2023-03-01 16:42:04 -0400

    Test: US2: implementing functionality of producer's adding product to post's Unit Test and Espresso tests are initialized

---
+++ app/src/androidTest/java/com/example/onlinebartertrader/ProviderProductAddEspressoTest.java
@@ -0,0 +1,9 @@
+package com.example.onlinebartertrader;
+
+/*
+* This Espresso Tests are testing the producer's product adding page.
+* Producer clicks "POST" button to add another product.
+* */
+
+public class ProviderProductAddEspressoTest {
+}

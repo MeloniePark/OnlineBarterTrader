beb9c22669e6c77ba26e9e07c62454e45a3f6ac8
EunBeen Park <en261649@dal.ca>
2023-03-06 14:47:40 -0400

    initial JUnit Test for US2 ProviderProductAddUnitTest.java completed

--- app/src/test/java/com/example/onlinebartertrader/ProviderProductAddUnitTest.java
+++ app/src/test/java/com/example/onlinebartertrader/ProviderProductAddUnitTest.java
@@ -20,6 +20,8 @@ package com.example.onlinebartertrader;
  */
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -42,7 +44,7 @@ public class ProviderProductAddUnitTest {
     @Test
     public void checkIfProductTypeFieldIsEmpty() {
         assertTrue(providerPostItemActivity.isProductTypeEmpty(""));
-        assertFalse(providerPostItemActivity.isProductTypeEmpty("baby toy"));
+        assertFalse(providerPostItemActivity.isProductTypeEmpty("baby toys"));
     }
 
     @Test
@@ -64,36 +66,53 @@ public class ProviderProductAddUnitTest {
 
     @Test
     public void checkIfDescriptionFieldIsEmpty(){
-
+        assertTrue(providerPostItemActivity.isDescriptionEmpty(""));
+        assertFalse(providerPostItemActivity.isDescriptionEmpty("This baby toy is squicky clean new!"));
     }
 
     @Test
     public void checkIfDateOfAvailabilityFieldIsEmpty(){
-
+        assertTrue(providerPostItemActivity.isDateEmpty(""));
+        assertFalse(providerPostItemActivity.isDateEmpty("2023-03-05"));
     }
 
     @Test
     public void checkIfDateOfAvailabilityIsValid(){
-
+        //
+        assertFalse(providerPostItemActivity.isDateValid("2022-03-02"));
+        assertFalse(providerPostItemActivity.isDateValid("2024-03-02"));
+        assertTrue(providerPostItemActivity.isDateValid(java.time.LocalDate.now()));
+        assertTrue(providerPostItemActivity.isDateValid("2023-04-12"));
+        assertFalse(providerPostItemActivity.isDateValid("02-03-2023"));
+        assertFalse(providerPostItemActivity.isDateValid("02-42-2023"));
+        assertFalse(providerPostItemActivity.isDateValid("this Friday"));
     }
 
     @Test
     public void checkIfPlaceOfExchangeIsEmpty(){
-
+        assertTrue(providerPostItemActivity.isPlaceOfExchangeEmpty(""));
+        assertFalse(providerPostItemActivity.isPlaceOfExchangeEmpty("Halifax City Hall"));
     }
 
     @Test
     public void checkIfApproxMarketValueFieldIsEmpty(){
-
+        assertTrue(providerPostItemActivity.isApproxMarketValueEmpty());
+        assertFalse(providerPostItemActivity.isApproxMarketValueEmpty(190.00));
     }
 
     @Test
     public void checkIfApproxMarketValueIsValid(){
-
+        assertFalse(providerPostItemActivity.isApproxMarketValueValid("twenty dollar"));
+        assertFalse(providerPostItemActivity.isApproxMarketValueValid(192,394.99));
+        assertFalse(providerPostItemActivity.isApproxMarketValueValid("negotiable"));
+        assertFalse(providerPostItemActivity.isApproxMarketValueValid("30 dollar"));
+        assertTrue(providerPostItemActivity.isApproxMarketValueValid(20.00));
+        assertTrue(providerPostItemActivity.isApproxMarketValueValid(70));
     }
 
     @Test
     public void checkIfPreferredExchangeInReturnFieldIsEmpty(){
-
+        assertTrue(providerPostItemActivity.isPreferredExchangeInReturnEmpty(""));
+        assertFalse(providerPostItemActivity.isPreferredExchangeInReturnEmpty("baby toys"));
     }
 }

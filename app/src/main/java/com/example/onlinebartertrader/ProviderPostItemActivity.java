f79c6d58cf2b806976db886d16995e470be91b12
EunBeen Park <en261649@dal.ca>
2023-03-06 15:04:11 -0400

    Implementation: impletented basic boolean functions for future function implementaiton. Currently onlys function structures are there.

--- app/src/main/java/com/example/onlinebartertrader/ProviderPostItemActivity.java
+++ app/src/main/java/com/example/onlinebartertrader/ProviderPostItemActivity.java
@@ -16,6 +16,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class ProviderPostItemActivity extends AppCompatActivity implements View.OnClickListener {
@@ -32,6 +33,45 @@ public class ProviderPostItemActivity extends AppCompatActivity implements View.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_provider);
     }
+
+
+    public boolean isProductTypeEmpty(String productType){
+        return false;
+    }
+
+    public boolean isProductTypeValid(String productType){
+        return false;
+    }
+
+    public boolean isDescriptionEmpty(String description){
+        return false;
+    }
+
+    public boolean isDateEmpty(String date){
+        return false;
+    }
+
+    public boolean isDateValid(String date){
+        return false;
+    }
+
+    public boolean isPlaceOfExchangeEmpty(String exchangePlace){
+        return false;
+    }
+
+    public boolean isApproxMarketValueEmpty(int marketValue){
+        return false;
+    }
+
+    public boolean isApproxMarketValueValid(int marketValue){
+        return false;
+    }
+
+    public boolean isPreferredExchangeInReturnEmpty(String preferredExchange){
+        return false;
+    }
+
+
 
 
     @Override

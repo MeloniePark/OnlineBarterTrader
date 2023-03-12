73651c069db364b308b92c7061c7f93078922f1b
Qiaodan Luo <qiaodan.luo@dal.ca>
2023-03-12 01:32:07 -0400

    Implemented the logic-related features

--- app/src/main/java/com/example/onlinebartertrader/ProviderPostItemActivity.java
+++ app/src/main/java/com/example/onlinebartertrader/ProviderPostItemActivity.java
@@ -10,14 +10,29 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.common.util.NumberUtils;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+import java.util.regex.Pattern;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class ProviderPostItemActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -26,6 +41,14 @@ public class ProviderPostItemActivity extends AppCompatActivity implements View.
     DatabaseReference providerDBRef;
 
 
+    //view for the lists
+    ListView ProductType;
+    ListView Description;
+    ListView Date;
+    ListView PlaceOfExchange;
+    ListView ApproxMarketValue;
+    ListView PreferredExchangeInReturn;
+    Button providerPostBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,38 +59,76 @@ public class ProviderPostItemActivity extends AppCompatActivity implements View.
 
 
     public boolean isProductTypeEmpty(String productType){
+        if (productType.isEmpty()) {
+            return true;
+        }
         return false;
     }
 
     public boolean isProductTypeValid(String productType){
+        String type1 = "furniture";
+        String type2 = "clothes";
+        String type3 = "computer accessories";
+        String type4 = "mobile phones";
+        String type5 = "baby toys";
+        if (productType.equalsIgnoreCase(type1)||
+                productType.equalsIgnoreCase(type2)||
+                productType.equalsIgnoreCase(type3)||
+                productType.equalsIgnoreCase(type4)||
+                productType.equalsIgnoreCase(type5))   {
+            return true;
+        }
         return false;
     }
 
     public boolean isDescriptionEmpty(String description){
+        if (description.isEmpty()) {
+            return true;
+        }
         return false;
     }
 
     public boolean isDateEmpty(String date){
+        if (date.isEmpty()) {
+            return true;
+        }
         return false;
     }
 
     public boolean isDateValid(String date){
+        Pattern pattern = Pattern.compile("^\\d{4}\\-(0?[1-9]|1[012])\\-(0?[1-9]|[12][0-9]|3[01])$");
+        if (pattern.matcher(date).matches()) {
+            return true;
+        }
         return false;
     }
 
     public boolean isPlaceOfExchangeEmpty(String exchangePlace){
+        if (exchangePlace.isEmpty()) {
+            return true;
+        }
         return false;
     }
 
-    public boolean isApproxMarketValueEmpty(int marketValue){
+    public boolean isApproxMarketValueEmpty(String marketValue){
+        if (marketValue.isEmpty()) {
+            return true;
+        }
         return false;
     }
 
-    public boolean isApproxMarketValueValid(int marketValue){
+    public boolean isApproxMarketValueValid(String marketValue){
+        Pattern pattern = Pattern.compile("[1-9][0-9]*");
+        if (pattern.matcher(marketValue).matches()) {
+            return true;
+        }
         return false;
     }
 
     public boolean isPreferredExchangeInReturnEmpty(String preferredExchange){
+        if (preferredExchange.isEmpty()) {
+            return true;
+        }
         return false;
     }
 

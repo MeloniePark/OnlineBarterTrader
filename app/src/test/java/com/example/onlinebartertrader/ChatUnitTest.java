969e814fcddbae1b68d6b984b175146f1f5dfed9
EunBeen Park <en261649@dal.ca>
2023-03-27 17:08:47 -0300

    Test: US1 JUnit Tests

--- app/src/test/java/com/example/onlinebartertrader/ChatUnitTest.java
+++ app/src/test/java/com/example/onlinebartertrader/ChatUnitTest.java
@@ -1,6 +1,7 @@
 package com.example.onlinebartertrader;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -12,8 +13,11 @@ import java.util.Date;
 
 public class ChatUnitTest {
 
+    static ChatActivity chatActivity;
+
     @BeforeClass
     public static void setup() {
+        chatActivity = new ChatActivity();
     }
 
     @AfterClass
@@ -21,20 +25,28 @@ public class ChatUnitTest {
         System.gc();
     }
 
-    //*** User story 1, AT?? **/
+    /** Iteration 3 User story 1, AT?? **/
     @Test
     public void checkRightProvider(){
-
+        //Please click magic mouse for this story.
+        assertTrue(chatActivity.checkProvider("mel@dalca"));
+        assertFalse(chatActivity.checkProvider(""));
+        assertFalse(chatActivity.checkProvider("test@dalca"));
     }
 
-
-    //*** User story 1, AT?? **/
+    /** Iteration 3 User story 1, AT?? **/
     @Test
     public void checkRightReceiver(){
-
+        //please log in using test@dal.ca for this unit test
+        assertTrue(chatActivity.checkReceiver("test@dalca"));
+        assertFalse(chatActivity.checkReceiver("mel@dalca"));
+        assertFalse(chatActivity.checkReceiver(""));
     }
 
-
-
-
+    /** Iteration 3 User story 1, AT?? **/
+    @Test
+    public void checkMessageEmpty(){
+        assertTrue(chatActivity.isMessageEmpty(""));
+        assertFalse(chatActivity.isMessageEmpty("baby toys"));
+    }
 }

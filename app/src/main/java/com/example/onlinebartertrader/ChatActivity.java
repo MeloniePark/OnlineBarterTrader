ddf877907589bdd24e0575d27ff9b8cb52c7069a
EunBeen Park <en261649@dal.ca>
2023-03-26 22:09:39 -0300

    US1 prep: basic file initialized - no implemtation yet, because of TDD functionality will be implemented after Tests implementation

--- app/src/main/java/com/example/onlinebartertrader/ChatActivity.java
+++ app/src/main/java/com/example/onlinebartertrader/ChatActivity.java
@@ -1,2 +1,44 @@
-package com.example.onlinebartertrader;public class ChatActivity {
+package com.example.onlinebartertrader;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.logging.*;
+
+
+public class ChatActivity extends AppCompatActivity{
+
+    public ChatActivity() {
+        System.out.println("I am in Chat Activity");
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+
+
+
+
+
 }

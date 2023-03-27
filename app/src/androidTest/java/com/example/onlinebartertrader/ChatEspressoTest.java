332ebb7050bfb87ca40a44140ffcc62f348336dc
EunBeen Park <en261649@dal.ca>
2023-03-26 22:03:25 -0300

    Test: US1 Test files initialized

--- app/src/androidTest/java/com/example/onlinebartertrader/ChatEspressoTest.java
+++ app/src/androidTest/java/com/example/onlinebartertrader/ChatEspressoTest.java
@@ -1,2 +1,31 @@
-package com.example.onlinebartertrader;public class ChatEspressoTest {
+package com.example.onlinebartertrader;
+
+import androidx.test.espresso.intent.Intents;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class ChatEspressoTest {
+
+    @Rule
+    public ActivityScenarioRule<ReceiverLandingPage> myRule = new ActivityScenarioRule<>(ReceiverLandingPage.class);
+    // We copy and pasted the template from assignment, but Intents rule is not used so far.
+
+    @BeforeClass
+    public static void setup() {
+        Intents.init();
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        System.gc();
+    }
+
 }
+
--- app/src/test/java/com/example/onlinebartertrader/ChatUnitTest.java
+++ app/src/test/java/com/example/onlinebartertrader/ChatUnitTest.java
@@ -1,2 +1,40 @@
-package com.example.onlinebartertrader;public class ChatUnitTest {
+package com.example.onlinebartertrader;
+
+import static org.junit.Assert.assertFalse;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ChatUnitTest {
+
+    @BeforeClass
+    public static void setup() {
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        System.gc();
+    }
+
+    //*** User story 1, AT?? **/
+    @Test
+    public void checkRightProvider(){
+
+    }
+
+
+    //*** User story 1, AT?? **/
+    @Test
+    public void checkRightReceiver(){
+
+    }
+
+
+
+
 }

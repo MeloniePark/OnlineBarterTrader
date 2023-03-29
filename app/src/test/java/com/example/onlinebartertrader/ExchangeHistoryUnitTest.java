7a07f485c1863d955b2e9b285d61c7df303bca1c
lu3 <xn967824@dal.ca>
2023-03-29 16:57:49 -0300

    Unit Test for the exchange history

---
+++ app/src/test/java/com/example/onlinebartertrader/ExchangeHistoryUnitTest.java
@@ -0,0 +1,59 @@
+package com.example.onlinebartertrader;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ExchangeHistoryActivityTest {
+
+    static ExchangeHistoryActivity exchangeHistoryActivity;
+
+    @BeforeClass
+    public static void setup() {
+        exchangeHistoryActivity = new ExchangeHistoryActivity();
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        System.gc();
+    }
+
+    @Test
+    public void receiverExchangeHistory_isDisplayed() {
+        String userRole = "Receiver";
+        String userId = "test@dalca";
+        String productName = "Test Product";
+        String dateOfPurchase = "2023-03-28";
+        String cost = "50";
+        String exchangeItem = "Test Exchange Item";
+        String location = "Test Location";
+        String providerId = "test@provider";
+
+        // Assuming you have a method in ExchangeHistoryActivity that sets the userRole and userId
+        exchangeHistoryActivity.setUserRoleAndId(userRole, userId);
+
+        // Assuming you have a method in ExchangeHistoryActivity to check if the correct data is displayed
+        assertTrue(exchangeHistoryActivity.isExchangeHistoryDisplayed(userRole, userId, productName, dateOfPurchase, cost, exchangeItem, location, providerId));
+    }
+
+    @Test
+    public void providerExchangeHistory_isDisplayed() {
+        String userRole = "Provider";
+        String userId = "test@dalca";
+        String productName = "Test Product";
+        String dateOfPurchase = "2023-03-28";
+        String cost = "50";
+        String exchangeItem = "Test Exchange Item";
+        String location = "Test Location";
+        String receiverId = "test@receiver";
+
+        // Assuming you have a method in ExchangeHistoryActivity that sets the userRole and userId
+        exchangeHistoryActivity.setUserRoleAndId(userRole, userId);
+
+        // Assuming you have a method in ExchangeHistoryActivity to check if the correct data is displayed
+        assertTrue(exchangeHistoryActivity.isExchangeHistoryDisplayed(userRole, userId, productName, dateOfPurchase, cost, exchangeItem, location, receiverId));
+    }
+}

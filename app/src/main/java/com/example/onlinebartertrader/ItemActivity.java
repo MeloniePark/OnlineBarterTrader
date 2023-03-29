99fcb97d286d760421e2b4c0c559563d8fd331f7
EunBeen Park <en261649@dal.ca>
2023-03-29 17:34:18 -0300

    Refactoring: itemActivity class's minor refactoring for better readability

--- app/src/main/java/com/example/onlinebartertrader/ItemActivity.java
+++ app/src/main/java/com/example/onlinebartertrader/ItemActivity.java
@@ -31,24 +31,11 @@ public class ItemActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item);
 
-        Intent intent = getIntent();
-        itemInformationView = (TextView)findViewById(R.id.productInformation);
-        transaction = (Button)findViewById(R.id.buyNowButton);
-        chatWithProvider = (Button)findViewById(R.id.chatWithProviderButton);
 
-        // Get item info from the intent sent from ReceiverItemList class
-        itemID = intent.getStringExtra("itemID");
-        itemName = intent.getStringExtra("itemName");
-        itemType = intent.getStringExtra("itemType");
-        itemDescription = intent.getStringExtra("description");
-        preferredExchange = intent.getStringExtra("preferredExchange");
-        providerEmail = intent.getStringExtra("providerEmail");
-        receiverEmail = intent.getStringExtra("receiverEmail");
+        getLayoutComponents();
+        getIntentItemInfo();
+        setItemInformation();
 
-        //Put the item information to the itemInformationView
-        String itemInfoString = "Item: " + itemName + "\nItem Type: " + itemType+ "\n\nDescription: "
-                + itemDescription + "\n\nPreferred Exchange Item: " + preferredExchange;
-        itemInformationView.setText(itemInfoString);
 
         //On Chat Button Click, switch to chat.
         chatWithProvider.setOnClickListener(new View.OnClickListener(){
@@ -64,5 +51,30 @@ public class ItemActivity extends AppCompatActivity {
         //On Buy now Button Click, switch to buy now.. - this will be implemented in US2
 
 
+    }
+
+    public void getLayoutComponents(){
+        itemInformationView = (TextView)findViewById(R.id.productInformation);
+        transaction = (Button)findViewById(R.id.buyNowButton);
+        chatWithProvider = (Button)findViewById(R.id.chatWithProviderButton);
+    }
+
+    public void getIntentItemInfo(){
+        Intent intent = getIntent();
+        // Get item info from the intent sent from ReceiverItemList class
+        itemID = intent.getStringExtra("itemID");
+        itemName = intent.getStringExtra("itemName");
+        itemType = intent.getStringExtra("itemType");
+        itemDescription = intent.getStringExtra("description");
+        preferredExchange = intent.getStringExtra("preferredExchange");
+        providerEmail = intent.getStringExtra("providerEmail");
+        receiverEmail = intent.getStringExtra("receiverEmail");
+    }
+
+    public void setItemInformation(){
+        //Put the item information to the itemInformationView
+        String itemInfoString = "Item: " + itemName + "\nItem Type: " + itemType+ "\n\nDescription: "
+                + itemDescription + "\n\nPreferred Exchange Item: " + preferredExchange;
+        itemInformationView.setText(itemInfoString);
     }
 }

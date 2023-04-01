909952a3848124158a3eb336e86649e3227e3d07
Reeti <reeti.shah@dal.ca>
2023-03-31 23:16:38 -0300

    JUnit tests for valuation

--- app/src/main/java/com/example/onlinebartertrader/UserStats.java
+++ app/src/main/java/com/example/onlinebartertrader/UserStats.java
@@ -1,4 +1,12 @@
 package com.example.onlinebartertrader;
 
 public class UserStats {
+
+    boolean checkGivenRating(String ratingFromDB, String rating) {
+        return ratingFromDB.equalsIgnoreCase(rating);
+    }
+
+    boolean checkTotalAmount(String valuationFromDB, String TotalValue) {
+        return valuationFromDB.equalsIgnoreCase(TotalValue);
+    }
 }

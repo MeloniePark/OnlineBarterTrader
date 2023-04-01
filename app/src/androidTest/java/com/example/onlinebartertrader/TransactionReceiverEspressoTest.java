c299dd0f2afda63ac3cb78b92ba431bcf9b80f64
EunBeen Park <en261649@dal.ca>
2023-04-01 00:53:59 -0300

    Test: Espressotest for the US2 Transaction initialized

--- app/src/androidTest/java/com/example/onlinebartertrader/TransactionReceiverEspressoTest.java
+++ app/src/androidTest/java/com/example/onlinebartertrader/TransactionReceiverEspressoTest.java
@@ -1,12 +1,20 @@
 package com.example.onlinebartertrader;
 
+import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.core.AllOf.allOf;
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertEquals;
 
 import android.content.Context;
@@ -33,9 +41,6 @@ public class TransactionReceiverEspressoTest {
     @Rule
     public ActivityScenarioRule<TransactionActivity> myRule = new ActivityScenarioRule<>(TransactionActivity.class);
     // We copy and pasted the template from assignment, but Intents rule is not used so far.
-    // Commented it out since it sometimes gives an error for now.
-    //public IntentsTestRule<MainActivity> myIntentRule = new IntentsTestRule<>(MainActivity.class);
-
 
     @BeforeClass
     public static void setup() {
@@ -50,7 +55,63 @@ public class TransactionReceiverEspressoTest {
 
     //*** User story 3, AT1 **/
     @Test
-    public void checkSomething() {
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.example.onlinebartertrader", appContext.getPackageName());
     }
+
+    @Test
+    public void checkIfTransactionPageIsVisible() {
+        onView(withId(R.id.receiverProductInExchange)).check(matches(isDisplayed()));
+        onView(withId(R.id.receiverTransactionEstCost)).check(matches(isDisplayed()));
+        onView(withId(R.id.chatRecyclerMessagesView)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void checkIfTransactionCanBeConfirmed() {
+        onView(withId(R.id.receiverProductInExchange)).perform(typeText("exchange product"));
+        onView(withId(R.id.receiverProductInExchange)).perform(closeSoftKeyboard());
+        onView(withId(R.id.receiverTransactionEstCost)).perform(typeText("77"));
+        onView(withId(R.id.receiverTransactionEstCost)).perform(closeSoftKeyboard());
+
+        onView(withId(R.id.receiverTransactionConfirmBtn)).perform(click());
+        intended(hasComponent(TransactionActivity.class.getName()));
+        onView(withId(R.id.receiverTransactionConfirmBtn)).check(matches(isClickable()));
+    }
+
+    @Test
+    public void checkIfReceiverExchangeProductEmpty() {
+        onView(withId(R.id.receiverProductInExchange)).perform(typeText(""));
+        onView(withId(R.id.receiverProductInExchange)).perform(closeSoftKeyboard());
+        onView(withId(R.id.receiverTransactionEstCost)).perform(typeText("77"));
+        onView(withId(R.id.receiverTransactionEstCost)).perform(closeSoftKeyboard());
+
+        onView(withId(R.id.receiverTransactionConfirmBtn)).perform(click());
+        onView(withId(R.id.transactionReceiverErrorMessage)).check(matches(withText(R.string.EMPTY_RECEIVER_EXCHANGE_PRODUCT)));
+    }
+
+    @Test
+    public void checkIfProductEstimatedCostEmpty() {
+        onView(withId(R.id.receiverProductInExchange)).perform(typeText("Melonie's tear"));
+        onView(withId(R.id.receiverProductInExchange)).perform(closeSoftKeyboard());
+        onView(withId(R.id.receiverTransactionEstCost)).perform(typeText(""));
+        onView(withId(R.id.receiverTransactionEstCost)).perform(closeSoftKeyboard());
+
+        onView(withId(R.id.receiverTransactionConfirmBtn)).perform(click());
+        onView(withId(R.id.transactionReceiverErrorMessage)).check(matches(withText(R.string.EMPTY_RECEIVER_EST_ITEM_COST)));
+    }
+
+    @Test
+    public void checkIfProductEstimatedCostInvalid() {
+        onView(withId(R.id.receiverProductInExchange)).perform(typeText("Melonie's tear"));
+        onView(withId(R.id.receiverProductInExchange)).perform(closeSoftKeyboard());
+        onView(withId(R.id.receiverTransactionEstCost)).perform(typeText("1000000000 dollar"));
+        onView(withId(R.id.receiverTransactionEstCost)).perform(closeSoftKeyboard());
+
+        onView(withId(R.id.receiverTransactionConfirmBtn)).perform(click());
+        onView(withId(R.id.transactionReceiverErrorMessage)).check(matches(withText(R.string.INVALID_RECEIVER_EST_ITEM_COST)));
+    }
+
 
 }

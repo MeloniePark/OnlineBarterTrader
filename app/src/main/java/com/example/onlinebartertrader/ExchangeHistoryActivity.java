d6a1796cf0f2b24fab5e9c1f0d39337f4b85e796
lu3 <xn967824@dal.ca>
2023-04-06 19:52:17 -0300

    Separating the provider from the recipient

--- app/src/main/java/com/example/onlinebartertrader/ExchangeHistoryActivity.java
+++ app/src/main/java/com/example/onlinebartertrader/ExchangeHistoryActivity.java
@@ -51,68 +51,14 @@ public class ExchangeHistoryActivity extends AppCompatActivity {
         userType = getIntent().getStringExtra("userType");
         userEmailAddress = getIntent().getStringExtra("emailAddress");
 
+        //Set up the exchange history list view
+        exchangeHistoryList = findViewById(R.id.exchange_history_recycler_view);
+
         //Set the user type and id
         setUserRoleAndId(userType,userEmailAddress);
 
         //Get a reference to the exchange history database node based on the user type
         setExchangeHistoryRef(userType,userEmailAddress,database);
-
-        //Set up the exchange history list view
-        exchangeHistoryList = findViewById(R.id.exchange_history_recycler_view);
-
-        //Attach a value event listener to the exchange history reference
-        exchangeHistoryRef.addValueEventListener(new ValueEventListener() {
-                @Override
-                public void onDataChange (@NonNull DataSnapshot dataSnapshot){
-                        //Create a list to hold the formatted exchange history strings
-                List<String> exchangeHistoryStrings = new ArrayList<>();
-
-                //Iterate through the exchange history items and create formatted strings
-                for (DataSnapshot itemSnapshot : dataSnapshot.getChildren()) {
-                    itemStatus = itemSnapshot.child("currentStatus").getValue(String.class);
-
-                    //Check if the itemStatus is Sold
-                    if (itemStatus != null && itemStatus.equals("Sold Out")) {
-                        productName = itemSnapshot.child("productName").getValue(String.class);
-                        transactionDate = itemSnapshot.child("transactionDate").getValue(String.class);
-                        cost = itemSnapshot.child("approxMarketValue").getValue(String.class);
-                        exchangeItem = itemSnapshot.child("preferredExchange").getValue(String.class);
-                        location = itemSnapshot.child("placeOfExchange").getValue(String.class);
-                        providerId = itemSnapshot.child("providerID").getValue(String.class);
-                        receiverId = itemSnapshot.child("receiverID").getValue(String.class);
-
-                        String itemDetails = "Product Name: " + productName +
-                                "\nTransaction Date: " + transactionDate +
-                                "\nCost: " + cost +
-                                "\nExchange Item: " + exchangeItem +
-                                "\nLocation: " + location;
-
-                        if (userType.equals("Provider")) {
-                            itemDetails += "\nReceiver ID/Username: " + receiverId;
-                        } else {
-                            itemDetails += "\nProvider ID/Username: " + providerId;
-                        }
-
-                        exchangeHistoryStrings.add(itemDetails);
-                    }
-                }
-                //Create an ArrayAdapter to display the exchange history in the ListView
-                ArrayAdapter<String> adapter = new ArrayAdapter<>(ExchangeHistoryActivity.this,
-                        android.R.layout.simple_list_item_1, exchangeHistoryStrings);
-
-                //Set the adapter for the ListView
-                    if (exchangeHistoryList != null) {
-                        exchangeHistoryList.setAdapter(adapter);
-                    } else {
-                        Log.e("ExchangeHistoryActivity", "exchangeHistoryList is null");
-                    }
-                }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                //Handle the database error
-            }
-        });
 
         //Get a reference to the back button
         backButton = findViewById(R.id.backToStat);
@@ -180,21 +126,122 @@ public class ExchangeHistoryActivity extends AppCompatActivity {
         return false;
     }
 
-
     protected void setStatusMessage(String message) {
         TextView statusLabel = findViewById(R.id.errorMessage);
         statusLabel.setText(message.trim());
     }
 
     public void setExchangeHistoryRef(String userType, String userEmailAddress, FirebaseDatabase database) {
+        //Check if the user is a Provider or Receiver and fetch the respective data
         if (userType.equals("Provider")) {
-            exchangeHistoryRef = database.getReference("Users").child("Provider").child(userEmailAddress).child("items");
+            exchangeHistoryRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    fetchProviderData(dataSnapshot);
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    //Handle the database error
+                }
+            });
         } else {
-            exchangeHistoryRef = database.getReference("Users").child("Receiver").child(userEmailAddress).child("items");
+            DatabaseReference providerItemsRef = database.getReference("Users").child("Provider");
+            providerItemsRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    for (DataSnapshot providerSnapshot : dataSnapshot.getChildren()) {
+                        DataSnapshot itemsSnapshot = providerSnapshot.child("items");
+                        fetchReceiverData(itemsSnapshot);
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    //Handle the database error
+                }
+            });
         }
     }
 
-    protected void switch2StatPage() {
+    private void fetchProviderData(DataSnapshot dataSnapshot) {
+        List<String> exchangeHistoryStrings = new ArrayList<>();
+
+        for (DataSnapshot itemSnapshot : dataSnapshot.getChildren()) {
+            itemStatus = itemSnapshot.child("currentStatus").getValue(String.class);
+
+            if (itemStatus != null && itemStatus.equals("Sold Out")) {
+                productName = itemSnapshot.child("productName").getValue(String.class);
+                transactionDate = itemSnapshot.child("transactionDate").getValue(String.class);
+                cost = itemSnapshot.child("approxMarketValue").getValue(String.class);
+                exchangeItem = itemSnapshot.child("preferredExchange").getValue(String.class);
+                location = itemSnapshot.child("placeOfExchange").getValue(String.class);
+                receiverId = itemSnapshot.child("receiverID").getValue(String.class);
+
+                String itemDetails = "Product Name: " + productName +
+                        "\nTransaction Date: " + transactionDate +
+                        "\nCost: " + cost +
+                        "\nExchange Item: " + exchangeItem +
+                        "\nLocation: " + location +
+                        "\nReceiver ID/Username: " + receiverId;
+
+                exchangeHistoryStrings.add(itemDetails);
+            }
+        }
+        updateAdapterWithData(exchangeHistoryStrings);
+    }
+
+    private void fetchReceiverData(DataSnapshot dataSnapshot) {
+        List<String> exchangeHistoryStrings = new ArrayList<>();
+
+        for (DataSnapshot itemSnapshot : dataSnapshot.getChildren()) {
+            receiverId = itemSnapshot.child("receiverID").getValue(String.class);
+
+            if (receiverId != null && receiverId.equals(userEmailAddress)) {
+                DatabaseReference providerItemsRef = itemSnapshot.getRef().getParent();
+
+                providerItemsRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot providerItemsSnapshot) {
+                        productName = providerItemsSnapshot.child("productName").getValue(String.class);
+                        transactionDate = providerItemsSnapshot.child("transactionDate").getValue(String.class);
+                        cost = providerItemsSnapshot.child("approxMarketValue").getValue(String.class);
+                        exchangeItem = providerItemsSnapshot.child("preferredExchange").getValue(String.class);
+                        location = providerItemsSnapshot.child("placeOfExchange").getValue(String.class);
+                        providerId = providerItemsSnapshot.child("providerID").getValue(String.class);
+
+                        String itemDetails = "Product Name: " + productName +
+                                "\nTransaction Date: " + transactionDate +
+                                "\nCost: " + cost +
+                                "\nExchange Item: " + exchangeItem +
+                                "\nLocation: " + location +
+                                "\nProvider ID/Username: " + providerId;
+
+                        exchangeHistoryStrings.add(itemDetails);
+                        updateAdapterWithData(exchangeHistoryStrings);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        // Handle the database error
+                    }
+                });
+            }
+        }
+    }
+
+    private void updateAdapterWithData(List<String> exchangeHistoryStrings) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(ExchangeHistoryActivity.this,
+                android.R.layout.simple_list_item_1, exchangeHistoryStrings);
+
+        if (exchangeHistoryList != null) {
+            exchangeHistoryList.setAdapter(adapter);
+        } else {
+            Log.e("ExchangeHistoryActivity", "exchangeHistoryList is null");
+        }
+    }
+
+        protected void switch2StatPage() {
         Intent intent = new Intent(this, UserStats.class);
         startActivity(intent);
     }

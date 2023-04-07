3ca8e6a235e7465df5ed473a431a905cb0980a69
EunBeen Park <en261649@dal.ca>
2023-04-07 15:23:07 -0300

    refactoring: refactoring and javadoc added to Chat and ChatActivity.java

--- app/src/main/java/com/example/onlinebartertrader/Chat.java
+++ app/src/main/java/com/example/onlinebartertrader/Chat.java
@@ -1,31 +1,65 @@
 package com.example.onlinebartertrader;
 
+/**
+ * Chat.java
+ *
+ * Description: Chat class is one of the supporting class for the instnat chat functionality
+ *          for the receiver and provider.
+ *          The Chat class provides user & chat message getter and setter for the ChatActivity.
+ *
+ * Reference:   Tutorial on Instant Chat Integration
+ */
 public class Chat {
     //    keys
     private String username;
     private String chatMessage;
-    //required empty constructor for firebase
-    public Chat() {
 
+    /**
+     * Chat():
+     * Firebase requires constructor, so created empty constructor
+     */
+    public Chat() {
     }
-    //initialising constructor
+
+    /**
+     * Chat(String username, String chatMessage) :
+     *      initialising constructor
+     * @param username  String chat user's name.
+     * @param chatMessage   String chatMessage user send.
+     */
     public Chat(String username, String chatMessage) {
         this.username = username;
         this.chatMessage = chatMessage;
     }
-    //getters and setters
+
+    /**
+     * getUsername(): Getter for username
+     * @return String username
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * setUsername(String username) : Setter for username
+     * @param username  String username
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * getChatMessage() : Getter for chat message
+     * @return String chatMessage
+     */
     public String getChatMessage() {
         return chatMessage;
     }
 
+    /**
+     * setChatMessage(String chatMessage) : setter for chatMessage
+     * @param chatMessage String chatMessage
+     */
     public void setChatMessage(String chatMessage) {
         this.chatMessage = chatMessage;
     }

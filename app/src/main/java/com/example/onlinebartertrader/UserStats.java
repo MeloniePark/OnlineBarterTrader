66777315832de833bf5c5c478fa4ca0bc7901a3b
shivang <sh573829@dal.ca>
2023-04-07 08:11:26 -0300

    Added code to display rating of Receiver

--- app/src/main/java/com/example/onlinebartertrader/UserStats.java
+++ app/src/main/java/com/example/onlinebartertrader/UserStats.java
@@ -1,12 +1,17 @@
 package com.example.onlinebartertrader;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.location.LocationListener;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.database.DataSnapshot;
@@ -22,12 +27,16 @@ public class UserStats extends AppCompatActivity implements View.OnClickListener
 
     //firebase
     FirebaseDatabase database;
-    DatabaseReference userDBRef;
+    DatabaseReference providerDBRef;
+    DatabaseReference receiverDBRef;
 
     String userEmailAddress;
     String userLoggedIn;
 
-    ArrayList<Integer> valuationList = new ArrayList<>();
+    ArrayList<Integer> valuationListProvider = new ArrayList<>();
+    ArrayList<Integer> valuationListReceiver = new ArrayList<>();
+    ArrayList<Float> ratingListProvider = new ArrayList<>();
+    ArrayList<Float> ratingListReceiver = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,34 +47,96 @@ public class UserStats extends AppCompatActivity implements View.OnClickListener
         userLoggedIn = getIntent().getStringExtra("userLoggedIn");
         userEmailAddress = getIntent().getStringExtra("emailAddress");
 
-        userDBRef = database.getReference("Users").child(userLoggedIn).child(userEmailAddress).child("items");
+        providerDBRef = database.getReference("Users").child("Provider").child(userEmailAddress).child("items");
+        receiverDBRef = database.getReference("Users").child("Provider");
 
-        valuationList = new ArrayList<>();
+        valuationListProvider = new ArrayList<>();
+        valuationListReceiver = new ArrayList<>();
 
-        userDBRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                for (DataSnapshot itemSnapshot : dataSnapshot.getChildren()) {
-                    String currentStatus = itemSnapshot.child("currentStatus").getValue(String.class);
-                    if (currentStatus != null && currentStatus.equals("Sold Out")) {
-                        String approxMarketValue = itemSnapshot.child("approxMarketValue").getValue(String.class);
-                        int value = Integer.parseInt(approxMarketValue);
-                        valuationList.add(value);
+        if(userLoggedIn.equals("Provider")){
+            providerDBRef.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    for (DataSnapshot itemSnapshot : dataSnapshot.getChildren()) {
+                        String currentStatus = itemSnapshot.child("currentStatus").getValue(String.class);
+                        if (currentStatus != null && currentStatus.equals("Sold Out")) {
+                            String approxMarketValue = itemSnapshot.child("approxMarketValue").getValue(String.class);
+                            int value = Integer.parseInt(approxMarketValue);
+                            valuationListProvider.add(value);
+                        }
+                    }
+                    int valuation = 0;
+                    for (int i = 0; i < valuationListProvider.size(); i++) {
+                        valuation += valuationListProvider.get(i);
+                    }
+                    TextView valuationView = findViewById(R.id.valuationView);
+                    valuationView.setText(String.valueOf(valuation));
+
+                    RatingBar ratingBar = findViewById(R.id.ratingBar);
+                    TextView ratingValueTextView = findViewById(R.id.rating);
+
+                    ratingBar.setRating(3.7f);
+                    ratingValueTextView.setText(String.format("%.1f", ratingBar.getRating()));
+                    ratingBar.setIsIndicator(true);
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+        }
+
+        else {
+            receiverDBRef.addValueEventListener(new ValueEventListener() {
+                @SuppressLint("DefaultLocale")
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    for (DataSnapshot providerSnapshot : dataSnapshot.getChildren()) {
+                        DataSnapshot itemsSnapshot = providerSnapshot.child("items");
+                        for (DataSnapshot itemSnapshot : itemsSnapshot.getChildren()) {
+                            String currentStatus = itemSnapshot.child("currentStatus").getValue(String.class);
+                            String receiverID = itemSnapshot.child("receiverID").getValue(String.class);
+                            if (currentStatus != null && receiverID != null && currentStatus.equals("Sold Out") && receiverID.equals(userEmailAddress)) {
+                                String approxMarketValue = itemSnapshot.child("receiverEnteredPrice").getValue(String.class);
+                                int value = Integer.parseInt(approxMarketValue);
+                                valuationListReceiver.add(value);
+
+                                String reciverRatinng = itemSnapshot.child("receiverRating").getValue(String.class);
+                                if (reciverRatinng != null){
+                                    float rating = Float.parseFloat(reciverRatinng);
+                                    ratingListReceiver.add(rating);
+                                }
+                            }
+                        }
+                        int valuation = 0;
+                        for (int i = 0; i < valuationListReceiver.size(); i++) {
+                            valuation += valuationListReceiver.get(i);
+                        }
+                        TextView valuationView = findViewById(R.id.valuationView);
+                        valuationView.setText(String.valueOf(valuation));
+
+                        int sum =0;
+                        for (int i = 0; i < ratingListReceiver.size(); i++) {
+                            sum += ratingListReceiver.get(i);
+                        }
+                        float mean = (float)sum / ratingListReceiver.size();
+                        RatingBar ratingBar = findViewById(R.id.ratingBar);
+                        TextView ratingValueTextView = findViewById(R.id.rating);
+
+                        ratingBar.setRating(mean);
+                        ratingValueTextView.setText(String.format("%.1f", mean));
+                        ratingBar.setIsIndicator(true);
                     }
                 }
-                int valuation = 0;
-                for (int i = 0; i < valuationList.size(); i++) {
-                    valuation += valuationList.get(i);
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
                 }
-                TextView valuationView = findViewById(R.id.valuationView);
-                valuationView.setText(String.valueOf(valuation));
-            }
+            });
+        }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
     }
 
     @Override

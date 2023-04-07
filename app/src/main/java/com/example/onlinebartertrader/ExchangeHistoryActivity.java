ab4c2109aa216e64a75c54cdbb988f5568c3fa4d
lu3 <xn967824@dal.ca>
2023-04-07 03:14:31 -0300

    change a bit

--- app/src/main/java/com/example/onlinebartertrader/ExchangeHistoryActivity.java
+++ app/src/main/java/com/example/onlinebartertrader/ExchangeHistoryActivity.java
@@ -307,9 +307,9 @@ public class ExchangeHistoryActivity extends AppCompatActivity {
     }
 
 /*   we do not have the return button for all pages, so I leave it here.
-//**
-     * Launches and switch the UserStats activity and finishes the current activity.
-     *//*
+
+     //Launches and switch the UserStats activity and finishes the current activity.
+
         protected void switch2StatPage() {
         Intent intent = new Intent(this, UserStats.class);
         startActivity(intent);

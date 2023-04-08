e2c5c8271b4fba210ff3bbd18b4a4189c707bc8c
EunBeen Park <en261649@dal.ca>
2023-04-07 23:19:24 -0300

    refactoring: SoldItemActivity.java javadoc completed

--- app/src/main/java/com/example/onlinebartertrader/SoldItemActivity.java
+++ app/src/main/java/com/example/onlinebartertrader/SoldItemActivity.java
@@ -8,6 +8,14 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+/**
+ * SoldItemActivity.java
+ *
+ * Description:
+ *  Activity to display the details of a sold item and allow the provider to
+ *  rate the receiver.
+ *
+ */
 public class SoldItemActivity extends AppCompatActivity{
 
     // Intent data declaration
@@ -29,6 +37,14 @@ public class SoldItemActivity extends AppCompatActivity{
     TextView itemInformationView;
     Button rateButton;
 
+    /**
+     * onCreate(...)
+     * This method is called when the activity is starting.
+     * It initializes the activity layout, gets layout components,
+     * gets the intent item information and sets the item information.
+     *
+     * @param savedInstanceState a Bundle object containing the activity's previously saved state.
+     */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
@@ -42,27 +58,36 @@ public class SoldItemActivity extends AppCompatActivity{
 
         rateButton = findViewById(R.id.rateTheReceiverButtonProviderItem);
         rateButton.setOnClickListener(new View.OnClickListener(){
+            /**
+             * Switches to the receiver's rating page
+             * Sends the intent items to the ReceiverRating.class
+             * @param v view
+             */
             @Override
             public void onClick(View v){
-                // TODO:Switch to the page for rating the receiver
+                // Switch to the page for rating the receiver
                 Intent intent = new Intent(SoldItemActivity.this,ReceiverRating.class);
                 intent.putExtra("receiverEmail",receiverEmail);
                 intent.putExtra("userEmailAddress",providerEmail.toLowerCase());
                 intent.putExtra("itemKey",itemID);
                 intent.putExtra("receiverRating",receiverRating);
                 startActivity(intent);
-                // For Ahmed working on Receiver Rating: Use this part to Switch to the page for rating the receiver
             }
         });
 
     }
 
-    // layout components
+    /**
+     * Initializes the layout components.
+     */
     public void getLayoutComponents(){
         itemInformationView = (TextView)findViewById(R.id.productInformationProviderItem);
         rateButton = (Button)findViewById(R.id.buyNowButton);
     }
 
+    /**
+     * Gets item information from the intent.
+     */
     public void getIntentItemInfo(){
         Intent intent = getIntent();
         // Get item info from the intent sent from ReceiverItemList class
@@ -81,6 +106,9 @@ public class SoldItemActivity extends AppCompatActivity{
         receiverRating  = intent.getStringExtra("receiverRating");;
     }
 
+    /**
+     * Sets and Displays the item information.
+     */
     public void setItemInformation(){
         //Put the item information to the itemInformationView
         String itemInfoString =
